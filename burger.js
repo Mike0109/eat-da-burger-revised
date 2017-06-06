c37ccad69ec289b395a2cc75ac1cdb0fce0b5420
Mike0109 <Mike0109@github.com>
2017-06-05 21:41:30 -0700

    done

---
+++ burger.js
@@ -0,0 +1,16 @@
+var orm = require('../config/orm.js');
+
+var burgers = {
+	create: function(col, vals, cb){
+		orm.create('burgers', col, vals, function(res){
+			cb(res);
+		});
+	},
+	update: function(objColVals, condition, cb){
+		orm.update('burgers', objColVals, condition, function(res){
+			cb(res);
+		});
+	}
+};
+
+module.exports = burgers;
